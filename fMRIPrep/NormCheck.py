8e060330ed89dcaf3955a69bd55b3b08b9743fbe
Haigler <haigler3@illinois.edu>
2021-08-03 13:52:11 -0500

    Add files via upload

---
+++ fMRIPrep/NormCheck.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#In terminal, call
+	#python NormCheck.py filepath/filename1.nii.gz
+	
+import sys
+import numpy as np
+import nibabel as nb
+
+
+def test(img):
+    zooms = np.array([img.header.get_zooms()[:3]])
+    A = img.affine[:3, :3]
+    
+    cosines = A / zooms
+    diff = A - cosines * zooms.T
+
+    return not np.allclose(diff, 0), np.max(np.abs(diff))
+
+
+if __name__ == "__main__":
+    for fname in sys.argv[1:]:
+        affected, severity = test(nb.load(fname))
+        print(f"{fname} is affected ({severity=})")
+        if affected:
+            print(f"{fname} is affected ({severity=})")
+
