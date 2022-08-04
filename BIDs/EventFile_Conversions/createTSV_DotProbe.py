a02f1060bedbe5d8efbea7190915942604926d75
Megan Finnegan <heuristicwondering@gmail.com>
2022-08-04 08:35:13 -0500

    add error check for trial_type mislabelling and correction for found errors in dotprobe conversion

--- BIDs/EventFile_Conversions/createTSV_DotProbe.py
+++ BIDs/EventFile_Conversions/createTSV_DotProbe.py
@@ -3,7 +3,7 @@
 # Only data deemed relevant for analysis have been retained, however this has been construed broadly.
 
 from os import listdir
-from os.path import isfile, join
+from os.path import isfile, join, exists
 import numpy as np
 import pandas
 
@@ -12,10 +12,17 @@ pandas.options.mode.chained_assignment = None
 
 
 class Data:
-    def __init__(self, dataFileName):
+    def __init__(self, dataFileName, errorFileName=None):
+        if errorFileName:
+            # for optional additional error checking
+            self.errorFileName = errorFileName
+            self.errorCheckData = self.__loadErrorData()
+        else:
+            self.errorCheckData = None
+
         self.contents = None
         self.dataFile = dataFileName
-        self.stimuli = [Probe()]
+        self.stimuli = [Probe(error_data=self.errorCheckData)]
         self.readFields = self.__declareReadFields()
         self.writeFields = self.__declareWriteFields()
 
@@ -30,6 +37,15 @@ class Data:
                        'congruency', 'word_onset', 'word_duration', 'probe_onset', 'probe_duration',
                        'wordL', 'wordR', 'trial_type', 'trial']
         return writeFields
+
+    def __loadErrorData(self):
+        assert exists('dotprobe-wordpairs.csv'), (
+            f"The file {self.errorFileName} required for extra error checking is missing. "
+            "See ./dotprobe-wordpairs-example.csv for how to construct this file. "
+            "Original file intentionally excluded for public sharing of code. "
+            "You may request this file if needed for reproducibility.")
+
+        return pandas.read_csv(self.errorFileName, sep='\t')
 
     def load(self):
         # These try-except blocks have been tailored to the particular data files being parsed in order to accommodate
@@ -65,7 +81,9 @@ class Data:
 
 # specific types of stimuli in the task
 class Probe:
-    def __init__(self):
+    def __init__(self, error_data=None):
+        self.error_data = error_data # optional additional error checking
+
         self.onset = None
         self.duration = None
         self.reaction_time = None
@@ -116,6 +134,24 @@ class Probe:
             if not timeNAmatches.all() or not scanTimeNAmatches.all():
                 raise Exception('Mismatch in blank entries for response and response timings (RT and/or scan time).')
 
+        if dataName == 'trial_type' and self.error_data is not None:
+            # Verify that the recorded word pairs match the preselected word pair list, then
+            # verify that the recorded trial_type matches the expected trial_type for each word pair
+            allowed_trials = self.error_data
+            for index, row in data.iterrows():
+                wordR_trials = allowed_trials.loc[(allowed_trials['word1'] == row['wordR']) |
+                                                  (allowed_trials['word2'] == row['wordR'])]
+                bothWord_trials = wordR_trials.loc[(wordR_trials['word1'] == row['wordL']) |
+                                                   (wordR_trials['word2'] == row['wordL'])]
+
+                wordpair = ' '.join(row[['wordR', 'wordL']].values.tolist())
+
+                if bothWord_trials.empty:
+                    raise Exception('Word pair ({}) not found in list of expected word pairs'.format(wordpair))
+
+                if not (bothWord_trials['valence'] == row['trial_type']).any():
+                    raise Exception('Valence (i.e. trial_type) of word pair ({}) does not match expected valence'.format(wordpair))
+
     def clean(self, rawData, outputFields):
         # combine onset data into one column
         onset = rawData['Words.OnsetTime'] - rawData['GetReady.OnsetTime']
@@ -157,7 +193,6 @@ class Probe:
         probe_rl = probe_rl.replace({'probe_rl': probe_rlDecodVals})
 
         # calculate accuracy
-        response.loc[1,'response'] = 'right' # debugging
         correct = response['response'] == probe_rl['probe_rl']
         accuracy = correct.to_frame('accuracy')
         accuracy = accuracy.where(correct, other="incorrect")
@@ -204,6 +239,16 @@ class Probe:
         trial_type = rawData[self.trial_typeField]
         trial_type = trial_type.rename(columns={trial_type.columns[0]: 'trial_type'})
 
+        # recoding the mislabelled trial_type
+        # Note that it was discovered that for SNAP 1 data, the trial_type for the word pair
+        # (worthy, stairs) was incorrectly encoded as neutral, when it should have been positive.
+        trial_type.loc[(wordR['wordR'] == 'worthy') &
+                       (wordL['wordL'] == 'stairs'), 'trial_type'] = 'pos'
+        trial_type.loc[(wordR['wordR'] == 'stairs') &
+                       (wordL['wordL'] == 'worthy'), 'trial_type'] = 'pos'
+
+        self.error_check('trial_type', pandas.concat([wordR, wordL, trial_type], axis=1))
+
         # extract trial number
         trial = rawData[self.trialField]
         trial = trial.rename(columns={trial.columns[0]: 'trial'})
@@ -238,10 +283,17 @@ snap1outdir = basefolder + "Converted Files/SNAP 1/DotProbe/"
 snap2outdir = basefolder + "Converted Files/SNAP 2/DotProbe/"
 snap3outdir = basefolder + "Converted Files/SNAP 3/DotProbe/"
 
+
+# optional file for additional error checking.
+# switch to commented code if not needed.
+#errorFileName = None
+errorFileName = './dotprobe-wordpairs.csv'
+
+
 # read, clean, and write data
 seenIDs = []
 for datafile in snap1files:
-    thisData = Data(join(snap1datadir, datafile))
+    thisData = Data(join(snap1datadir, datafile), errorFileName)
     subjID = str(''.join(filter(str.isdigit, datafile)))
 
     try:
